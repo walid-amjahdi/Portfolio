5c766c29881af02458161b471669e8a0de877875
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-09-16 20:51:42 +0100

    dev the educ comp

--- src/Component/Educations.jsx
+++ src/Component/Educations.jsx
@@ -1,10 +1,83 @@
+import '../Styles/Education.css'
+
 export function Educations()
 {
     return (
         <>
+        <div className='educ-comp'>
             <h3>Educations</h3>
-                <h5>Licence in Information Systems Engineering</h5>
+            <div className='sections'>
+                <div className="title">
+                    <h4>Licence in Information Systems Engineering</h4>
+                    <p>Expected 2027</p>
+                </div>
+                    <p>
+                        Faculté des Sciences Semlalia (FSSM), Université Cadi Ayyad, Marrakech
+                        <br />
+                        Broad training in software development, full-stack development, databases,
+                        machine learning, and mathematics
+                    </p>
+            </div>
+             <div className='sections'>
+                <div className="title">
+                    <h4>DEUG in Applied Computer Science (Informatique Appliquée)</h4>
+                    <p>2026</p>
+                </div>
+                    <p>
+                        Faculté des Sciences Semlalia (FSSM), Université Cadi Ayyad, Marrakech
+                    </p>
+            </div>
+             <div className='sections'>
+                <div className="title">
+                    <h4>Baccalaureate in Physical Sciences – BIOF</h4>
+                    <p>2023</p>
+                </div>
+            </div>
+
             <h3>Certifications</h3>
+            <div className="sections">
+                <div className="title">
+                    <h4>Machine Learning with Tree-Based Models in Python </h4> 
+                    <p>DataCamp</p>
+                </div>
+            </div>
+            <div className="sections">
+                <div className="title">
+                    <h4>Introduction to Data Science in Python</h4> 
+                    <p>DataCamp</p>
+                </div>
+            </div>
+            <div className="sections">
+                <div className="title">
+                    <h4>Data Manipulation with pandas</h4> 
+                    <p>DataCamp</p>
+                </div>
+            </div>
+            <div className="sections">
+                <div className="title">
+                    <h4>Cluster Analysis in Python</h4> 
+                    <p>DataCamp</p>
+                </div>
+            </div>
+            <div className="sections">
+                <div className="title">
+                    <h4>Data Structures and Algorithms in Python</h4> 
+                    <p>DataCamp</p>
+                </div>
+            </div>
+            <div className="sections">
+                <div className="title">
+                    <h4>Intermediate SQL</h4> 
+                    <p>DataCamp</p>
+                </div>
+            </div>
+            <div className="sections">
+                <div className="title">
+                    <h4>Intermediate GitHub Concepts</h4> 
+                    <p>DataCamp</p>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
