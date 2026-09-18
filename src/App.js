1e3cd03bd9194d17da4b6de592077c9d428e0a83
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-09-18 21:23:57 +0100

    configure github pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <div>
-        <BrowserRouter>
+        <BrowserRouter basename='/Portfolio'>
           <Header/>
           <Main/>
         </BrowserRouter>
