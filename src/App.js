9cffff1d083fa6cf20031746f0aff41ce072e9c4
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-09-18 21:14:53 +0100

    fix a app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './Styles/App.css';
 import { Header } from './Component/Header';
 import { Main } from './Component/Main';
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
